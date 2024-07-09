db31320ce2531f6c3dbaa3446b5dc96e307ec598
Adeiye Oluwasegun <126037893+SenatorP03@users.noreply.github.com>
2024-07-09 21:04:46 +0100

    Update Contact_me.py

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,21 +1,16 @@
-
 import streamlit as st
 from send_email import send_email
 
 st.header("Contact Me")
 
 
-
 with st.form(key="emai_forms"):
     user_email = st.text_input("enter an email address")
-    opinion = st.selectbox("Comment", df["topic"])
-
     raw_message = st.text_area("Your comment here")
     message = (f"""\
 Subject:New email from {user_email}
-
-{user_email}
-{opinion}
+    
+From: {user_email}
 {raw_message}
 """)
     button = st.form_submit_button("Submit")
